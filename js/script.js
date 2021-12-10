831aec38c6e807c3522b25e6f6ba1ad5cf8fafe3
Dmitrii Medvedkin <d.medvedkin@avm-t.ru>
2021-12-10 17:53:17 +0300

    add function

--- js/script.js
+++ js/script.js
@@ -10,26 +10,24 @@ const personalMovieDB = {
     privat: false
 };
 
-// const countFilm = personalMovieDB.count;
+const countFilm = personalMovieDB.count;
 
-
-
-// if (countFilm < 10) {
-//     alert('Просмотрено довольно мало фильмов');
-// } else if (countFilm <= 30) {
-//     alert('Вы классический зритель');
-// } else if (countFilm > 30) {
-//     alert('Вы киноман');
-// } else {
-//     alert('Произошла ошибка');
-// }
+if (countFilm < 10) {
+    alert('Просмотрено довольно мало фильмов');
+} else if (countFilm <= 30) {
+    alert('Вы классический зритель');
+} else if (countFilm > 30) {
+    alert('Вы киноман');
+} else {
+    alert('Произошла ошибка');
+}
 
 
 for (let i = 0; i < 2; i++) {
     const question = prompt('Один из последних просмотренных фильмов?', ''),
           answer = prompt('На сколько оцените его?', '');
 
-    if (question != null && answer != null && question != '' && answer != '' && question.length < 50 ) {
+    if (question != null && answer != null && question != '' && answer != '' && question.length < 50) {
         personalMovieDB.movies[question] = answer;
         console.log('done');
     } else {
@@ -39,6 +37,73 @@ for (let i = 0; i < 2; i++) {
 
 }
 
-
-
 console.log(personalMovieDB);
+
+
+
+// //  Классический if else
+
+// if (num < 49) {
+//     console.log('Error');
+// } else if (num > 100) {
+//     console.log('Много');
+// } else {
+//     console.log('ok!');
+// }
+
+// (num === 50) ? console.log('ok'): console.log('Error');
+
+
+// // Условие Switch case
+
+// const num = 50;
+
+// switch (num) {
+//     case 49:
+//         console.log('No!');
+//         break;
+//     case 100:
+//         console.log('No!');
+//         break;
+//     case 50:
+//         console.log('Ok');
+//         break;
+//     default:
+//         console.log('No yet');
+//         break;
+// }
+
+// // Циклы
+
+// // While
+
+// let num = 50
+
+// while (num<55) {
+//     console.log(num);
+//     num++;
+// }
+
+// // Do While
+
+// do {
+//     console.log(num);
+//     num++;
+// }
+// while (num < 55);
+
+// // For
+
+// for (let i = 1; i < 8; i++) {
+//     if(i ===6) {
+//         continue;
+//     }
+
+//     console.log(i);
+// }
+
+function showFirstMessage(text) {
+    console.log(text);
+}
+
+showFirstMessage('Что вам тут наааадо?');
