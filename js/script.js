c3f11494a3091ea586ac603f260c1bef4c15c7c2
Dmitriy Medvedkin <dmitriyphc@gmail.com>
2022-01-10 22:47:47 +0300

    add object

--- js/script.js
+++ js/script.js
@@ -137,3 +137,41 @@ writeYourGenres();
 // }
 
 // showFirstMessage('Что вам тут наааадо?');
+
+
+const options = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        bg: 'red'
+    },
+    makeTest: function(){
+        console.log('Test');
+    }
+};
+
+options.makeTest();
+
+console.log(Object.keys(options).length);
+
+const{border, bg} = options.colors;
+
+console.log(border);
+
+// let counter = 0;
+
+// for (let key in options) {
+//     if (typeof(options[key]) === 'object' ) {
+//         for (let i in options[key]) {
+//             console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+//             counter++;
+//         }
+//     } else {
+//         console.log(`Свойство ${key} имеет значение ${options[key]}`);
+//         counter++;
+//     }
+// }
+
+// console.log(counter);
