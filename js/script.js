66b96f8541e61903959f11dd6bcef5847509d283
Dmitriy Medvedkin <dmitriyphc@gmail.com>
2022-05-15 01:42:19 +0300

    for lesson

--- js/script.js
+++ js/script.js
@@ -107,7 +107,7 @@ writeYourGenres();
 
 // // While
 
-// let num = 50
+// let num = 50;
 
 // while (num < 55) {
 //     console.log(num);
@@ -201,4 +201,41 @@ const beer = true,
 
 if (beer && hamburger) {
     console.log('Бургеров возьмём, пивка, посмеёмся, и всем проблемам пиздец');
-}      
+}
+
+// let result = '';
+
+// const rowCount = 7;
+
+// for (let i = 2; i < rowCount; i++) {
+    
+//     for (let j = 0; j < i; j++) {
+//         result += "*";
+//     }
+
+//     result += '\n';
+// }
+
+// console.log(result);
+
+
+first: for (let i = 0; i < 3; i++) {
+    console.log(`first level ${i}`);
+    for (let j = 0; j < 3; j++) {
+        console.log(`second level ${j}`);
+       for (let k = 0; k < 3; k++) {
+           if(k===2) {continue first;}
+        console.log(`third level ${k}`);
+       }
+    }
+}
+
+{
+   const lines = 6;
+   const result = '';
+
+   for (let i = 0; i < lines; i++) {
+       
+   }
+}
+
