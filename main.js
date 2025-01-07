0c383bd6a9da03b6f6efcc45c97986aadd97313c
DavePhc <dmitriyphc@gmail.com>
2025-01-07 17:58:05 +0300

    lesson #13 - done

--- main.js
+++ main.js
@@ -51,85 +51,85 @@
 
 // Задание по массивам #11
 
-const family = ['Peter', 'Ann', 'Alex', 'Linda'];
+// const family = ['Peter', 'Ann', 'Alex', 'Linda'];
 
-function showFamily(arr) {
+// function showFamily(arr) {
     
-    let str = "Семья пуста"
+//     let str = "Семья пуста"
     
-    if (arr.length > 0) {
-        str = "Семья состоит из: ";
-        arr.forEach(item => {
-            str += `${item} `;
-        })
-    }
+//     if (arr.length > 0) {
+//         str = "Семья состоит из: ";
+//         arr.forEach(item => {
+//             str += `${item} `;
+//         })
+//     }
     
-    return console.log(str);
+//     return console.log(str);
+// }
+
+// showFamily(family);
+
+// const favoriteCities = ['liSBon', 'ROME', 'miLan', 'Dublin'];
+
+// function standardizeStrings(arr) {
+//     arr.forEach(city => {
+//         console.log(`${city.toLowerCase()}`);
+//     })
+// }
+
+// standardizeStrings(favoriteCities);
+
+// Упрожнение по объектам и массивам #13
+
+// const shoppingMallData = {
+//     shops: [
+//         {
+//             width: 10,
+//             length: 5
+//         },
+//         {
+//             width: 15,
+//             length: 7
+//         },
+//         {
+//             width: 20,
+//             length: 5
+//         },
+//         {
+//             width: 8,
+//             length: 10
+//         }
+//     ],
+//     height: 5,
+//     moneyPer1m3: 30,
+//     budget: 50000,
+// }
+
+// function isBudgetEnough(data) {
+
+//     let totalSquare = 0,
+//         totalVolume = 0;
+
+//     data.shops.forEach(shop => {
+//         totalSquare += shop.width * shop.length;
+//     })
+
+//     totalVolume = totalSquare * data.height;
+
+//     if (data.budget >= totalVolume * data.moneyPer1m3) {
+//         return console.log(String("Бюджета достаточно")); 
+//     } else {
+//         return console.log(String("Бюджета недостаточно"));
+//     }
+    
+// }
+
+// isBudgetEnough(shoppingMallData);
+
+// Упрожнение по объектам и массивам #14
+
+const students = ['Peter', 'Andrew', 'Ann', 'Mark', 'Josh', 'Sandra', 'Cris', 'Bernard', 'Takesi', 'Sam'];
+
+function sortStudentsByGroups(arr) {
+
 }
-
-showFamily(family);
-
-const favoriteCities = ['liSBon', 'ROME', 'miLan', 'Dublin'];
-
-function standardizeStrings(arr) {
-    arr.forEach(city => {
-        console.log(`${city.toLowerCase()}`);
-    })
-}
-
-standardizeStrings(favoriteCities);
-
-// Упрожнение по объектам и массивам #13 
-
-const shoppingMallData = {
-    shops: [
-        {
-            width: 10,
-            length: 5
-        },
-        {
-            width: 15,
-            length: 7
-        },
-        {
-            width: 20,
-            length: 5
-        },
-        {
-            width: 8,
-            length: 10
-        }
-    ],
-    height: 5,
-    moneyPer1m3: 30,
-    budget: 50000,
-    calculatingTotalArea: function () {
-        let totalArea = 0;
-        this.shops.forEach((item) => {
-            totalArea += item.width * item.length;
-        })
-        return Number(totalArea);
-    },
-    calculatingTotalVolume: function () {
-        let totalVolume = this.calculatingTotalArea() * this.height;
-        return totalVolume;
-    },
-    calculatingMonthlyBudget: function () {
-        this.totalBudget = this.calculatingTotalVolume() * this.moneyPer1m3;
-    }
-}
-
-shoppingMallData.calculatingTotalArea();
-shoppingMallData.calculatingTotalVolume();
-shoppingMallData.calculatingMonthlyBudget();
-
-
-function isBudgetEnough(data) {
-    if (data.totalBudget <= data.budget) {
-        return String("Бюджета достаточно");
-    } else {
-        return String("Бюджета недостаточно");
-    }
-}
-
-isBudgetEnough(shoppingMallData);
